c9aba84af505249ae75d5677fb718ca04edf2d88
Justine C <blackwolfman581@gmail.com>
2025-08-05 20:52:52 +0200

    Import Furniture Battle

--- Source/WarForApple/Private/Interface_NPC.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interface_NPC.h"
-
-// Add default functionality here for any IInterface_NPC functions that are not pure virtual.
--- Source/WarForApple/WarForApple.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WarForApple.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WarForApple, "WarForApple" );
