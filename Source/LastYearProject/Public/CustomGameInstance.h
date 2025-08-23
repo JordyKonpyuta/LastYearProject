b2335fbae9c3b6e63dca463d6f3d25fcb3181bf2
Justine C <blackwolfman581@gmail.com>
2025-08-23 18:49:09 +0200

    Link SAA and FB on console + Set up Game instance + Set up videos

---
+++ Source/LastYearProject/Public/CustomGameInstance.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CustomGameInstance.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS()
+class LASTYEARPROJECT_API UCustomGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
